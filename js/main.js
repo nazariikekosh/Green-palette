b4a021fd94973a8506daf3b43ae9fa2ae680b2bc
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2024-10-27 15:11:36 +0200

    add__burger-btn

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 const swiper = new Swiper('.reviews__slider', {
-
   slidesPerView: 2,
   spaceBetween: 22,
   loop: true,
 
   navigation: {
-    nextEl: '.reviews__item-prev',
-    prevEl: '.reviews__item-next',
-  },
+    nextEl: '.reviews__item-next',
+    prevEl: '.reviews__item-prev',
+  }
+});
+
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu--open');
+  menuBtn.classList.toggle('menu__btn--open');
 });
